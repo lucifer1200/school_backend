19f19204fe0ace841c2bf3e22a6340f07b4117f1
jain1 <jain12harsh@gmail.com>
2025-05-29 01:42:37 +0530

    delete

--- API-Services/src/main/java/com/org/apiservices/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.org.apiservices.service;
-
-import com.org.apiservices.dto.CommonResponse;
-
-public interface EmailService {
-
-	CommonResponse sendEmail(String to, String subject, String text);
-
-}
--- API-Services/src/main/java/com/org/apiservices/service/PushNotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.org.apiservices.service;
-
-import com.org.apiservices.dto.CommonResponse;
-
-public interface PushNotificationService {
-
-	CommonResponse sendPushNotification(String token, String title, String body);
-
-}
